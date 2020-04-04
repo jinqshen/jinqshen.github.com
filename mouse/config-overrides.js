c8975f9b6e35b8e14cbe2b7e7c75538d802a3168
jinqshen <554695481@qq.com>
2020-04-04 17:39:24 +0800

    add less loader

--- mouse/config-overrides.js
+++ mouse/config-overrides.js
@@ -1,9 +1,18 @@
-const { override, fixBabelImports } = require('customize-cra');
+const { override, fixBabelImports, addLessLoader, addWebpackPlugin } = require('customize-cra');
+const AntdDayjsWebpackPlugin = require('antd-dayjs-webpack-plugin');
+
+process.env.GENERATE_SOURCEMAP = "false";
 
 module.exports = override(
     fixBabelImports('import', {
         libraryName: 'antd',
         libraryDirectory: 'es',
-        style: 'css',
+        style: true,
     }),
+    addLessLoader({
+        //为支持新版less-loader的配置方式，对于源码有所修改  modules:{localIdentName: '[path][name]__[local]--[hash:base64:5]'}
+        javascriptEnabled: true,
+        modifyVars: {}
+    }),
+    addWebpackPlugin(new AntdDayjsWebpackPlugin())
 );
